d9bc3975763d46cf135f1556902dbb6a2ae8fcd7
GeorgSeng <georg.sengstbratl@gmail.com>
2025-09-11 19:24:09 +0300

    Add basic code for service2

--- service2/src/main/java/org/demo/LogResource.java
+++ service2/src/main/java/org/demo/LogResource.java
@@ -5,12 +5,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
+@Path("/log")
+public class LogResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String log() {
         return "Hello from Quarkus REST";
     }
 }
---
+++ service2/src/main/java/org/demo/StatusResource.java
@@ -0,0 +1,24 @@
+package org.demo;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.io.File;
+import java.time.temporal.ChronoUnit;
+
+@Path("/status")
+public class StatusResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String status() {
+       long jvmStartTime = java.lang.management.ManagementFactory.getRuntimeMXBean().getStartTime();
+       var startTime = java.time.Instant.ofEpochSecond(jvmStartTime);
+       var currentTime = java.time.Instant.now();
+       java.time.Duration duration = java.time.Duration.between(startTime, currentTime);
+       var usableSpace = new File("/").getUsableSpace() / (1024 * 1024); // convert bytes to mb
+       return String.format("%s: uptime %s hours, free disk in root: %d MBytes", currentTime.truncatedTo(ChronoUnit.SECONDS), usableSpace);
+    }
+}
